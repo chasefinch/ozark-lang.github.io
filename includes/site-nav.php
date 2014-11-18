e6b67cb6506c9ea5ae4e6f072f34c64e4b1f985d
Chase Finch <chase@engeniusweb.com>
2014-11-18 08:46:13 -0800

    Optic, plus correct commenting

--- includes/site-nav.php
+++ includes/site-nav.php
@@ -1,5 +1,5 @@
 <div class="site-nav">
   <ul>
-    <li<?php if($site == 'ozark') echo " class='active'"; ?>><a href="/">Ozark</a></li><li<?php if($site == 'ozark-ide') echo " class='active'"; ?>><!--<a href="/">Krazo</a></li><li<?php if($site == 'company') echo " class='active'"; ?>><a href="/">Purple Puppy</a></li>--> | <li><a target="_blank" href="http://eepurl.com/56iVb">Newsletter &nbsp;<span class='glyphicon glyphicon-new-window'></span></a></li>
+    <li<?php if($site == 'ozark') echo " class='active'"; ?>><a href="/">Ozark</a></li><!--<li<?php if($site == 'optic') echo " class='active'"; ?>><a href="/">Optic</a></li>--> | <li><a target="_blank" href="http://eepurl.com/56iVb">Newsletter &nbsp;<span class='glyphicon glyphicon-new-window'></span></a></li>
   </ul>
 </div>
